b6bf2cc2d4ded8b72bd857f2ff0e7f36dfc23545
Gupta <ashigupta5@deloitte.com>
2024-03-26 23:46:36 +0530

    Episode-01

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const heading = React.createElement("h1", {
+    id: 'heading',
+    class: 'h1',
+    title: 'h1 tag'
+}, "This is react heading");
+ReactDOM.createRoot(document.getElementById('root')).render(heading);
